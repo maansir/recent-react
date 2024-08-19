4e7b9dbdc3e8785177922f070dc5a7d5e7842656
maansir <m.farman4k@gmail.com>
2024-08-19 23:14:13 +0300

    ep-1

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement('div', {className:'farman'}, React.createElement('h1', {className:'headings'}, 'how are you React app'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
